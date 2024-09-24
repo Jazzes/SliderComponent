3707e037292107a3071343ed1350085415b07503
Boris <j4zzes@gmail.com>
2024-09-25 00:27:56 +0300

    done

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Technical task
+# Slider component
+
+### Склонируйте репозиторий:
+
+```
+git clone https://github.com/Jazzes/SliderComponent.git
+cd SliderComponent
+```
+
+### Подключите библиотек:
+
+```
+npm i
+```
+
+### Запустите проект:
+
+```
+npm start
+```
+
+### Для сборки проекта используйте:
+
+```
+npm run build
+```
--- src/component/timeBlock/timeBlockSwitcher/TimeBlockSwitch.tsx
+++ src/component/timeBlock/timeBlockSwitcher/TimeBlockSwitch.tsx
@@ -17,14 +17,14 @@ const TimeBlockSwitch: FC<ITimeBlockSwitcher> = ({changeIndexInterval, index, to
             <div className={styles.arrowContainer}>
                 <div className={styles.button + " " + (index === 0 ? styles.notActiveButton : "")}
                      onClick={() => {changeIndexInterval(index - 1)}}>
-                    <svg className={styles.arrowSVG} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M12.25 4L6 10.25L12.25 16.5" stroke="#42567A" strokeWidth="2"/>
                     </svg>
                 </div>
 
                 <div className={styles.button + " " + (index + 1 === totalRanges ? styles.notActiveButton : "")}
                      onClick={() => {changeIndexInterval(index + 1)}}>
-                    <svg className={styles.arrowSVG} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M7.75 16L14 9.75L7.75 3.5" stroke="#42567A" strokeWidth="2"/>
                     </svg>
 
